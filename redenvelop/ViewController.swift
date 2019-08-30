9b112cb628dd69a53409b6027be5016e5564a0b8
ChaosTong <easyulife@gmail.com>
2019-08-30 13:53:05 +0800

    Initial Commit

---
+++ redenvelop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  redenvelop
+//
+//  Created by chaostong on 2019/8/30.
+//  Copyright © 2019 chaostong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
